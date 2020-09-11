29e2d191356e211e0f09f3d698f5441222472c5e
shams II <rntaboada@yahoo.com.ar>
2020-09-11 00:30:02 -0300

    Add another view

--- urls.py
+++ urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from Proyecto1.views import saludo
+from Proyecto1.views import despedida
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/', saludo),
+    path('nosveremos/', despedida),
 ]
--- views.py
+++ views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def saludo(request): #es nuestra primera vista
     return HttpResponse("Hola alumnos esta es nuestra primer página con Django")
+
+def despedida(request):
+    return HttpResponse('Hasta luego alumnos de Django')
